09262ba999c38d82f81e18d68cf6d21278bb417e
Ruijian Cao <caoruijian@big.one>
2023-08-03 14:48:42 +0800

    add base user auth

--- lib/slink_web/controllers/page_controller.ex
+++ lib/slink_web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule SlinkWeb.PageController do
   use SlinkWeb, :controller
 
+  def index(conn, _params) do
+    render(conn, :index)
+  end
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
--- lib/slink_web/router.ex
+++ lib/slink_web/router.ex
@@ -1,6 +1,8 @@
 defmodule SlinkWeb.Router do
   use SlinkWeb, :router
 
+  import SlinkWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule SlinkWeb.Router do
     plug :put_root_layout, html: {SlinkWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -17,7 +20,7 @@ defmodule SlinkWeb.Router do
   scope "/", SlinkWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
@@ -39,6 +42,44 @@ defmodule SlinkWeb.Router do
 
       live_dashboard "/dashboard", metrics: SlinkWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", SlinkWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{SlinkWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", SlinkWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{SlinkWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", SlinkWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{SlinkWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
